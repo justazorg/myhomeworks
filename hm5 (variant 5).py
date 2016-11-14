82db31abe0f231352086deed8c7fc90f593b1f77
justazorg <muhnikmuhnik@gmail.com>
2016-11-14 21:07:44 +0300

    Delete hm5 (variant 5).py

--- hm5 (variant 5).py
+++
@@ -1,9 +0,0 @@
-arr = []
-word = input('latin word please:')
-while word:
-    arr.append(word)
-    word = input('latin word please:')
-for w in arr:
-    if w.strip('\t') in w:
-        print(w)
-        
